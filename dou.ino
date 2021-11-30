4579e68808fdd52a32e06862695487fe53458701
lq-jw <83740897+lq-jw@users.noreply.github.com>
2021-11-30 11:51:43 +0800

    Add files via upload

---
+++ dou.ino
@@ -0,0 +1,30 @@
+#include <BluetoothSerial.h>
+BluetoothSerial BT;
+int ledPin = 12;
+int ledPinb = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  BT.begin("doudouTestEsp32");//請改名
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (BT.available()) {
+    //讀取藍芽資料
+    String BTdata=BT.readString();
+    //顯示在序列視窗
+    Serial.println(BTdata);
+    if(BTdata=="a"){
+           digitalWrite(ledPin,HIGH);
+      }
+          if(BTdata=="b"){
+           digitalWrite(ledPinb,HIGH);
+      }
+
+  }
+  delay(1);
+}
